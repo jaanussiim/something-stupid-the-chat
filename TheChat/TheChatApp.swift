fdb8ad56e5dc61d3924dd9e3928a43f803d8dfb9
Jaanus Siim <jaanus@jaanussiim.com>
2022-11-11 06:37:43 +0200

    Initial Commit

---
+++ TheChat/TheChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheChatApp.swift
+//  TheChat
+//
+//  Created by Jaanus Siim on 11.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TheChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
